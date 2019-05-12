5c16872e666df302469f93a42d1aea9d4d75ae15
Ethan Brierley <ethanboxx@gmail.com>
2019-05-12 18:23:02 +0100

    Cli args support

---
+++ src/Cli.hs
@@ -0,0 +1,22 @@
+
+module Cli
+    ( getUserName
+    )
+where
+
+import           System.Environment
+import           System.IO
+
+getUserName :: IO String
+getUserName = do
+    args <- getArgs
+    case maybeHead args of
+        Just x -> pure x
+        Nothing ->
+            putStr "What github user do you want to clone: "
+                >> hFlush stdout
+                >> getLine
+
+maybeHead :: [a] -> Maybe a
+maybeHead (x : xs) = Just x
+maybeHead []       = Nothing
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,63 @@
-
 module Lib
-    ()
+    ( mainTask
+    )
 where
+
+import           Cli
+import qualified Data.ByteString.Lazy.Char8    as LC
+import           Data.Text                     as T
+import           Data.Aeson
+import           Network.HTTP.Simple
+import           GHC.Generics
+import           System.Process
+import           System.Exit
+import           Data.Maybe
+
+
+type RepoList = [Repo]
+
+data Repo = Repo
+    { name      :: T.Text
+    , language  :: Maybe T.Text
+    , clone_url :: T.Text
+    } deriving (Show, Generic)
+instance FromJSON Repo
+
+
+mainTask :: IO ()
+mainTask = do
+    responce <- getUserName >>= openRepoList
+    responce <- case responce of
+        Just a  -> pure a
+        Nothing -> error "Invalid JSON"
+    cloneAll responce >>= print
+
+
+cloneAll :: RepoList -> IO ExitCode
+cloneAll (x : xs) = do
+    print (command x)
+    currentCommand <- system (T.unpack (command x))
+    case currentCommand of
+        ExitSuccess   -> cloneAll xs
+        ExitFailure i -> pure (ExitFailure i)
+cloneAll [] = pure ExitSuccess
+
+
+command :: Repo -> T.Text
+command x = mconcat ["git clone ", clone_url x, " ", repoLanguage, "/", name x]
+  where
+    repoLanguage = T.map (\x -> if x == ' ' then '-' else x)
+                         (fromMaybe "other" (language x))
+
+openRepoList :: String -> IO (Maybe RepoList)
+openRepoList user =
+    (openRepoListJson user) >>= (pure . decodeRepoList . getResponseBody)
+
+openRepoListJson :: String -> IO (Response LC.ByteString)
+openRepoListJson user = do
+    request <- addRequestHeader "User-Agent" "git-backup" <$> parseRequest
+        (mconcat ["https://api.github.com/users/", user, "/repos"])
+    httpLBS request
+
+decodeRepoList :: LC.ByteString -> Maybe RepoList
+decodeRepoList = decode
